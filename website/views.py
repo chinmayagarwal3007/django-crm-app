6718f5831af816956fc073304d47281eaecb4cda
Chinmay <agarwalchinmay3007@gmail.com>
2025-03-14 22:13:37 +0530

    add login and register

--- website/views.py
+++ website/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.shortcuts import redirect
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            messages.success(request, "You have logged in successfully!")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error logging in! Please try again.")
+            return redirect('home')
     return render(request, 'home.html')
 
-def login_user(request):
-    pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have logged out successfully!")
+    return redirect('home')
+
+def register(request):
+    form = UserRegistrationForm()
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            messages.success(request, "You have registered successfully!")
+            return redirect('home')        
+    return render(request, 'registration/register.html', {'form':form})
